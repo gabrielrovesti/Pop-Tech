e56e19838c892085d78574d68061d7445015fe9f
Gabriel Rovesti <95100089+gabrielrovesti@users.noreply.github.com>
2022-11-21 15:55:51 +0100

    Sistemazione pagina Chi Siamo

--- sito/chiSiamo.php
+++ sito/chiSiamo.php
@@ -1,49 +1,43 @@
-<html>
+<!DOCTYPE html>
+<html lang="it">
     <head>
         <meta charset="utf-8">
         <title>Chi siamo</title>
         <meta name="description" content="Chi Siamo | PopTech">
         <meta name="keywords" content="PopTech, videogiochi, manga, giochi da tavolo, storia">
         <meta name="author" content="PopTech">
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../styles/style.css" media="screen">
-        <link rel="stylesheet" href="../styles/print.css" media="print">
-        <link rel="stylesheet" href="../styles/mobile.css" media="screen and (max-width: 480px)">
+
+        <link rel="stylesheet" href="styles/style.css" media="handheld,screen"  /> 
+        <link rel="stylesheet" href="styles/mobile.css" media="screen and (max-width: 600px), only screen and (max-device-width: 600px)" />
+        <link rel="stylesheet" href="styles/print.css" media="print" />
+        
         <link rel="shortcut icon" href="../images/favicon.ico">
     </head>
     <body>
-        <header>
-            <!-- Include PHP header file-->
-            <?php include 'includes/header.php'; ?>
-        </header>
-        <nav id="menu">
-            <!-- Include PHP menu file-->
-            <?php include 'includes/menu.php'; ?>
-        </nav>
+        <?php include "includes/header.php"; ?>
+        <?php include "includes/menu.php"; ?>
        
-        <main>
+        <nav id="breadcrumbs">
+        <p>Ti trovi in: Chi siamo</p> 
+        </nav>  
 
-        <h1>Chi siamo</h1>
+        <main id="content">
+            <h1>Chi siamo</h1>
 
-        <section class="container">
-            <div class="left-half">
-                <article>
-                    <img src="images/chisiamo1.png" class="image" alt="Storia dell'azienda">
-                </article>
-            </div>
-
-            <div class="right-half">
-                <article>
-                    <h2>Storia e Dove Siamo</h2>
+            <div class="container">
                 <p>
-                    La nostra azienda nasce nel 2001, da un gruppo di amici colleghi di università.
-                    Ben presto, si è capito che oltre al comune interesse per l'informatica, c'era molto di più
-                    che ci univa: la passione per i videogiochi, i manga, i giochi da tavolo e la storia.
-                    Intuendo le potenzialità di Internet, abbiamo deciso di creare un sito web per condividere
-                    le nostre passioni con il resto del mondo. Così è nata PopTech, che oggi è diventata un
-                    punto di riferimento per gli appassionati del settore, sia in Italia che all'estero.
+                <h2>Storia e Dove Siamo</h2>
+                    <img src="images/chisiamo1.png" class="img-left" alt="Storia dell'azienda">  
+                        La nostra azienda nasce nel 2001, da un gruppo di amici colleghi di università.
+                        Ben presto, si è capito che oltre al comune interesse per l'informatica, c'era molto di più
+                        che ci univa: la passione per i videogiochi, i manga, i giochi da tavolo e la storia.
+                        Intuendo le potenzialità di Internet, abbiamo deciso di creare un sito web per condividere
+                        le nostre passioni con il resto del mondo. Così è nata PopTech, che oggi è diventata un
+                        punto di riferimento per gli appassionati del settore, sia in Italia che all'estero.
                 </p>
-    
+
                 <p>
                     I tempi cambiano, ma la nostra passione rimane la stessa. Per questo, abbiamo deciso di
                     ampliare progressivamente la nostra offerta, così come i nostri interessi, per venire incontro
@@ -59,39 +53,82 @@
                     abbiamo incluso anche un laboratorio di videogiochi, dove poter testare i
                     nostri prodotti prima di metterli in vendita.
                 </p> 
-                </article>
-            </div>
-        </section>
-
-        <section class="container">
-            <div class="left-half">
-                <article>
-                    <h2>Il nostro <span lang="en">staff</span></h2>
-                    <p>
-                        Per noi, il lavoro è un gioco e la passione è il nostro motore.
-                        Per questo, abbiamo scelto di lavorare in un ambiente giovane e dinamico,
-                        dove il contatto con le persone e lo scambio di consigli, opinioni e idee
-                        è alla base della nostra idea di lavoro.
-                        Le persone contano per noi, e per questo abbiamo creato un team di
-                        professionisti altamente qualificati, che si occupano di ogni aspetto
-                        della nostra attività.
-                    </p>
-                </article>
             </div>
 
-            <div class="right-half">
-                <article>
-                    <img src="images/chisiamo2.png" class="image" alt="Luogo dell'azienda">
-                </article>
+            <div class="container">
+                <p>
+                <h2>Il nostro <span lang="en">staff</span></h2>
+                <img src="images/chisiamo2.png" class="img-right" alt="Staff dell'azienda">  
+                Per noi, il lavoro è un gioco e la passione è il nostro motore.
+                Per questo, abbiamo scelto di lavorare in un ambiente giovane e dinamico,
+                dove il contatto con le persone e lo scambio di consigli, opinioni e idee
+                è alla base della nostra idea di lavoro.
+                Le persone contano per noi, e per questo abbiamo creato un team di
+                professionisti altamente qualificati, che si occupano di ogni aspetto
+                della nostra attività.
+                </p>
+
+                <p>
+                    Il nostro team è composto da programmatori, grafici, web designer,
+                    copywriter, esperti di marketing e comunicazione, esperti di videogiochi,
+                    esperti di manga e giochi da tavolo, esperti di storia e molto altro.
+                    Tutti insieme, siamo in grado di offrire un servizio completo e di qualità,
+                    che soddisfa le esigenze di ogni cliente.
+                </p>
+
+                <h3 style="clear:both; text-align: center;">Staff Amministrativo</h3>
+                <p style="text-align: center">
+                La nostra azienda fonda le proprie radici su 4 persone, che si occupano
+                di ogni aspetto della nostra attività. Siamo il cuore pulsante di PopTech,
+                e siamo sempre pronti a rispondere alle vostre domande.
+                </p>
+
+                <ul class="membristaff">
+
+                    <li class="membro">
+                        <img src="images/gabriel.png" alt="">
+                        <h3>Gabriel Rovesti</h3>
+                        <p>
+                            Cerco di essere un ragazzo creativo, curioso e sempre pronto a inseguire l'ultima sfida.
+                            Seguo la parte responsabile e amministrativo "dietro le quinte",
+                            ma sono anche il responsabile della parte grafica e del marketing.
+                        </p>
+                    </li>
+
+                    <li class="membro">
+                        <img src="images/giulio.png" alt="">
+                        <h3>Giulio Moretto</h3>
+                        <p>
+                            Ho sempre avuto una passione per la tecnologia e la programmazione.
+                            Mi occupo della parte tecnica del sito, della gestione dei server e della sicurezza.
+                            Seguo inoltre la parte abbigliamento e videogiochi alla vendita diretta
+                        </p>
+                    </li>
+
+                    <li class="membro">
+                        <img src="images/erica.png" alt="">
+                        <h3>Erica Cavaliere</h3>
+                        <p>
+                            Sono una ragazza curiosa, che cerca sempre di divertirsi in quello che fa,
+                            e di imparare sempre qualcosa di nuovo. Mi occupo della parte commerciale,
+                            della gestione dei clienti e della vendita diretta.
+                        </p>
+                    </li>
+
+                    <li class="membro">
+                        <img src="images/teo.png" alt="">
+                        <h3>Teo Corbu</h3>
+                        <p>
+                            Cerco sempre di essere un ragazzo positivo e disponibile, e di fare sempre
+                            il possibile per aiutare gli altri. Mi occupo principalmente di seguire
+                            la parte dei videogiochi, con particolare occhio alle ultime tecnologie.
+                        </p>
+                    </li>
+
+                </ul>
             </div>
-        </section>
-            
+
         </main>
-        <footer>
-            <!-- Include PHP footer file-->
-            <?php 
-                include ('includes/footer.php');
-            ?>
-        </footer>
+        <?php include ('includes/footer.php');?>
     </body>
 </html>
