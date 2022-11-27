6b8ef025439151848e84086a283fae36604b96f0
teo-corbu <117911725+teo-corbu@users.noreply.github.com>
2022-11-27 14:36:08 +0100

    FAQ

---
+++ sito/FAQ.php
@@ -0,0 +1,63 @@
+﻿ <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+
+
+    <title>FAQ</title>
+        <meta name="description" content="FAQ | PopTech">
+        <meta name="keywords" content="PopTech, videogiochi, manga, giochi da tavolo, storia">
+        <meta name="author" content="PopTech">
+
+   
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="styles/style.css" media="handheld,screen"  /> 
+        <link rel="stylesheet" href="styles/mobile.css" media="screen and (max-width: 600px), only screen and (max-device-width: 600px)" />
+        <link rel="stylesheet" href="styles/print.css" media="print" />
+        
+        <link rel="shortcut icon" href="../images/favicon.ico">
+</head>
+<body>
+	
+    <?php include "includes/header.php"; ?>
+    <?php include "includes/menu.php"; ?>
+
+     <nav id="breadcrumbs">
+        <p>Ti trovi in: <span lang="en">Home</span> > <abbr="Frequently Asked Questions"> FAQ</abbr></p> 
+    </nav>
+
+
+
+
+
+    <main id="content">
+            <h1><abbr="Frequently Asked Questions"> FAQ</abbr></h1>
+
+            <h2>Ogni quanto ricevete prodotti dal Giappone?</h2>
+                <p>
+                    Le spedizioni dall'Asia arrivano ogni 11 del mese.
+                      
+                </p>
+
+                <h2>Ogni quanto ricevete prodotti dagli Stati Uniti?</h2>
+                <p>
+                    Le spedizioni dagli Stati Uniti arrivano ogni settimana.
+                </p>
+    
+                <h2>Spedite gli ordini in Italia?</h2>
+                <p>
+                    Spediamo con BRT in tutta Italia
+                </p> 
+            
+             <!-- DA CONTINUARE!!!!!-->
+
+
+        </main>
+
+
+
+
+    <?php include "includes/footer.php"; ?>
+</body>
+</html>
