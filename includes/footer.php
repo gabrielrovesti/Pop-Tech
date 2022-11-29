d2cf139d70e41a95cbc6b112bfb7778752b6d646
Giulio Moretto - Promocare <giulio@promocare.it>
2022-11-29 18:05:42 +0100

    Tolto il link "torna su" nascosto
    Tolto display:none; dal link torna alla pagina.
    Viene ignorato dallo screen reader e non visto dall'utente.

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<div class="nascosto"><a href="#">Torna all'inizio della pagina</a></div>
+<div><a href="#">Torna all'inizio della pagina</a></div>
 <footer>
 Tech Shop - Via della Prova 12 12345 Padova (PD) - P.Iva 0112345
 </footer>
