44bfab906c7734fdb39ed92ece22c8581a4998fe
Giulio Moretto - Promocare <giulio@promocare.it>
2022-12-31 15:13:41 +0100

    Header SVG

--- prodotto.php
+++ prodotto.php
@@ -25,7 +25,7 @@
 
                 $product = $products[0];
 
-                $breadcrumbs = '<p>Ti trovi in: <a href="/" lang="en">Home</a> > Prodotto > '.parse_lang($product['nome']).'</p> ';
+                $breadcrumbs = '<p>Ti trovi in: <a href="/" lang="en">Home</a> >  <a href="prodotti.php">Prodotti</a> > '.parse_lang($product['nome']).'</p> ';
                 
                 $title = parse_lang($product['nome'],true) . ' - Pop Tech';
 
