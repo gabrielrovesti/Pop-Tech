2d03ac5c08c9611ad83ecc89c966c9203bd3fb7d
Giulio Moretto - Promocare <giulio@promocare.it>
2023-01-02 09:44:22 +0100

    Elenco categorie in prodotti.php

--- prodotti.php
+++ prodotti.php
@@ -18,10 +18,16 @@
     if ($connection->open_connection()) {
         
         $categories = $connection->exec_select_query('SELECT id, nome FROM categoria ORDER BY nome;');
+
+        $content .= '<div class="categoyList">';
+        foreach($categories as $category){
+            $content .= '<a href="#cat-'.$category['id'].'" class="button">'.parse_lang($category['nome']).'</a>';
+        }
+        $content .= '</div>';
        
         foreach($categories as $category){
 
-            $content .= '<div class="comic_box"><h2 class="categoryTitle">'.parse_lang($category['nome']).'</h2> <a href="categoria.php?id='.$category['id'].'" class="button">Vedi Tutti</a></div>';
+            $content .= '<div class="comic_box" id="cat-'.$category['id'].'"><h2 class="categoryTitle">'.parse_lang($category['nome']).'</h2> <a href="categoria.php?id='.$category['id'].'" class="button">Vedi Tutti</a></div>';
 
             $products = $connection->exec_select_query('SELECT id, nome, immagine, altimmagine, descrizione, origine, marca, modello, dimensione, peso, categoria, prezzo FROM prodotto WHERE categoria='.$category['id'].';');
 
